4bf3b318759863cd54d7e741cbe0025304792c15
PowerKeysolutions <powerkeysolutions@users.noreply.github.com>
2026-05-18 21:15:20 +0200

    Security: remove hoja-de-ruta.md (contained credentials+IPs), sanitize deploy.sh

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-NUC="root@NUC-IP"
+# Configure your NUC address before running (Tailscale IP or LAN IP)
+NUC="${NUC_HOST:-root@<NUC-IP>}"
 REMOTE="/home/rokid/desktop-server"
 
 echo "Desplegando en NUC..."
